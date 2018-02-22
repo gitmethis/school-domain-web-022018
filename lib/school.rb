f2ef7f3b5e02accfaa616c1653fc92da6111c919
Chase <chaseit12@gmail.com>
2018-02-22 19:24:30 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
+require 'pry'
 # code here!
+
+class School
+   def initialize(name)
+    @name = name
+    @roster = {}
+   end
+   
+   def add_student(student, grade)
+     # first, make the array 
+     if !@roster[grade]
+        @roster[grade] = []     
+     end
+      @roster[grade].push(student)
+     # then, add the student
+     
+   end
+   
+   def grade(grade)
+       
+   end
+   
+   def roster
+       @roster
+   end
+end
