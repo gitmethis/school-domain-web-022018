71c5d3081f5e3a28d3ef8e85b2b62e484d5b435c
Chase <chaseit12@gmail.com>
2018-02-22 19:42:20 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
+require 'pry'
 # code here!
+
+class School
+   def initialize(name)
+    @name = name
+    @roster = {}
+   end
+   
+   def add_student(student, grade)
+     # first, make the array 
+     if !@roster[grade]
+        @roster[grade] = []     
+     end
+      @roster[grade].push(student)
+     # then, add the student
+     
+   end
+   
+   def grade(grade)
+       @roster[grade]
+   end
+   
+   def roster
+       @roster
+   end
+   
+   def sort
+      @roster = Hash[@roster.sort_by{
+        |key, value|
+         key
+      }]
+      @roster.each{ |key,value|
+         value = value.sort!
+      }
+      @roster
+   end
+end
